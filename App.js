4b68d4c35060642153b23b82928dab9e0e33e485
Shubham02022002 <chaudhary2001shubham@gamil.com>
2023-12-27 22:46:59 +0530

    "learning Components"

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const heading= React.createElement("h1",{id:"title"},"heading1");
+const heading = React.createElement(
+  "h1",
+  { id: "title", key: "h1" },
+  "heading1"
+);
+const heading2 = React.createElement(
+  "h2",
+  { id: "title", key: "h2" },
+  "heading2"
+);
 
-const heading2= React.createElement("h2",{id:"title"},"heading2");
+console.log(heading);
+const Heading3=()=>(
+    <h1 id="heading3" key={"h3"}>
+        Nameste React
+      </h1>
+)
 
-const container=React.createElement("div",{id:"container"},[heading,heading2]);   
-const container2=React.createElement("span",{id:"container"},[heading,heading2]);   
+const HeaderComponent = () => {
+  return (
+    <div>
+    <Heading3/>
+      <h2 id="newHeading" key={"hn"}>
+        Inside React Element
+      </h2>
+    </div>
+  );
+};
 
-const mc=React.createElement("div",{id:"mc"},[container,container2]);
-    
-const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(mc); 
+// const container=React.createElement("div",{id:"container",key:"c1"},[heading,heading2]);
+// const container2=React.createElement("span",{id:"container",key:"c2"},[heading3]);
+
+// const mainChild=React.createElement("div",{id:"mainChild"},[container,container2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent />);
