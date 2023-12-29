a1eff4d6ae0705ab53d230263fb281b9d849410b
Shubham02022002 <chaudhary2001shubham@gamil.com>
2023-12-29 22:26:03 +0530

    added fetch

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import { resturantList } from "../config";
 import ResturantCard from "./RresturantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 
 function filterData(searchText, resturants) {
   return resturants.filter((resturant) =>
@@ -10,10 +11,37 @@ function filterData(searchText, resturants) {
 const Body = () => {
   const [resturants, setResturants] = useState(resturantList);
   const [searchText, setSearchText] = useState("");
+
+  useEffect(()=>{
+    getResturants();
+  },[]);
+
+  // async function getResturants(){
+  //   const data=fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING");
+  //   const json= (await data).json();
+  //   console.log(json);
+  //   setResturants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  // }
+  async function getResturants(){
+    try {
+      const response = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING");
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const json = await response.json();
+      // console.log(json);
+      setResturants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  }
+  
+
   return (
     <>
       <div className="search-container">
         <input
+          id="search-field"
           className="search-input"
           type="text"
           placeholder="Serach"
